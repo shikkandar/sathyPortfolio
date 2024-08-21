c5bc21afccf35cbe0209640d8ab7dfa42e50aca6
shikkandar <mohammedshikkandar1@gmail.com>
2024-08-21 23:49:34 +0530

    update

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,24 +1,13 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
   DiReact,
   DiNodejs,
   DiMongodb,
-  DiPython,
   DiGit,
-  DiJava,
 } from "react-icons/di";
-import {
-  SiRedis,
-  SiFirebase,
-  SiNextdotjs,
-  SiSolidity,
-  SiPostgresql,
-  SiStrapi,
-} from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
+import { SiStrapi } from "react-icons/si";
 
 function Techstack() {
   return (
